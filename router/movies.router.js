e32662f9a52eed479b38fd5a20705ea0cfcf73b6
Rafael Basterra Careaga <rbasterra@msn.com>
2022-01-29 18:55:06 +0100

    add Get endpoint to search by movieId

--- router/movies.router.js
+++ router/movies.router.js
@@ -10,6 +10,26 @@ moviesRouter.get('/', (_req,res,next)=>{
             error.status = 500;
             return next(error);
         });
-})
+});
+
+moviesRouter.get('/:id', (req,res,next) =>{
+    const id = req.params.id;
+
+    return Movie.findById(id)
+        .then(movie => {
+            if (!movie){
+                error = new Error('Pelicula no encontrada');
+                error.status=404;
+                return next(error);
+            }
+            return res.status(200).json(movie);
+        })
+        .catch(err =>{
+            error = new Error(err);
+            error.status = 500;
+            return next(error);
+        });
+
+});
 
 module.exports = moviesRouter;

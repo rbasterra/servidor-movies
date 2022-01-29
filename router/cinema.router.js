b1f8bd517263fb371bf28375fbaa8da649652fa4
Rafael Basterra Careaga <rbasterra@msn.com>
2022-01-29 21:25:35 +0100

    add PUT endpoint to update cinemas and movies

--- router/cinema.router.js
+++ router/cinema.router.js
@@ -65,4 +65,35 @@ cinemaRouter.post('/',(req,res,next) =>{
         });
 });
 
+cinemaRouter.put('/:id', (req,res,next) =>{
+    const id = req.params.id;
+
+    Cinema.findByIdAndUpdate(id, {$set:req.body}, {new:true})
+        .then(cinema => {
+            if (!cinema){
+                error = new Error('Cine no encontrado');
+                error.status=404;
+                return next(error);
+            }
+            return res.status(200).json(cinema)
+        })
+        .catch(err =>{
+            const error = new Error(err);
+            error.status = 500;
+            return next(error);
+        });
+});
+
+cinemaRouter.delete('/:id',(req,res,next) =>{
+    const id = req.params.id;
+
+    Cinema.findByIdAndDelete(id)
+        .then(() => res.status(200).json(`Cine con id ${id} eliminado`))
+        .catch(err =>{
+            const error = new Error(err);
+            error.status = 500;
+            return next(error);
+        });
+})
+
 module.exports = cinemaRouter;
